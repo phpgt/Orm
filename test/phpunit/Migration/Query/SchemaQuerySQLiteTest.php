3db257ae97211ea9b5836555220d955ee58d50df
Greg Bowler <greg.bowler@g105b.com>
2025-01-14 14:35:58 +0000

    wip: tweak test layout

--- test/phpunit/Migration/Query/SchemaQuerySQLiteTest.php
+++ test/phpunit/Migration/Query/SchemaQuerySQLiteTest.php
@@ -31,8 +31,51 @@ class SchemaQuerySQLiteTest extends SQLTestCase {
 
 		$sut = new SchemaQuerySQLite($schemaTable);
 
+		$expected = <<<SQL
+		create table `TestTable` (
+			`id` int not null primary key,
+			`name` text null
+		)
+		SQL;
+
 		self::assertSameSQL(
-			"create table `TestTable` ( `id` int not null primary key, `name` text null )",
+			$expected,
+			$sut->generateSql(),
+		);
+	}
+
+	public function testGenerateSql_autoIncrement():void {
+		$field1 = self::createMock(SchemaField::class);
+		$field1->method("getName")->willReturn("id");
+		$field1->method("getType")->willReturn("int");
+		$field1->method("isNullable")->willReturn(false);
+		$field2 = self::createMock(SchemaField::class);
+		$field2->method("getName")->willReturn("name");
+		$field2->method("getType")->willReturn("string");
+		$field2->method("isNullable")->willReturn(true);
+
+		$fieldList = [$field1, $field2];
+
+		$schemaTable = self::createMock(SchemaTable::class);
+		$schemaTable->method("getName")
+			->willReturn("TestTable");
+		$schemaTable->method("getPrimaryKey")
+			->willReturn($field1);
+
+		$schemaTable->method("getFieldList")
+			->willReturn($fieldList);
+
+		$sut = new SchemaQuerySQLite($schemaTable);
+
+		$expected = <<<SQL
+		create table `TestTable` (
+			`id` int not null primary key,
+			`name` text null
+		)
+		SQL;
+
+		self::assertSameSQL(
+			$expected,
 			$sut->generateSql(),
 		);
 	}
